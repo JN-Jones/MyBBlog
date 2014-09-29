758560f3382553bb40bcef65c10e1f7640d7db0d
dthiago <thiagoalves@live.com>
2014-09-29 17:33:29 -0300

    Create mybblog.lang.php

---
+++ inc/languages/ptbr/mybblog.lang.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *Traduzido por dthiago ArnoldLayne-xXx  http://bf4brasil.com.br/
+ */
+ 
+$l['mybblog'] = "Blog";
+$l['mybblog_deactivated'] = "MyBBlog está desativado";
+
+$l['mybblog_write'] = "Escrever Novo Artigo";
+$l['edit'] = "Editar";
+
+$l['mybblog_articles_none'] = "Sem artigos para procurar";
+$l['mybblog_posted'] = "Criado{1} por {2}";
+$l['mybblog_comments'] = "{1} Comentários";
+$l['mybblog_tags'] = "Etiquetado por  {1}";
+
+$l['mybblog_new_article'] = "Escrever Novo Artigo";
+$l['mybblog_edit_article'] = "Editar Artigo";
+$l['mybblog_title'] = "Título";
+$l['mybblog_article_tags'] = "Etiquetas";
+$l['mybblog_tags_desc'] = "Separados por vírgulas";
+$l['mybblog_article'] = "Artigo";
+$l['mybblog_article_submit'] = "Salvar Artigo";
+$l['mybblog_article_written'] = "O artigo foi salvo!";
+
+$l['mybblog_article_comments'] = "Comentários";
+
+$l['mybblog_new_comment'] = "Escrever um novo comentário";
+$l['mybblog_edit_comment'] = "Editar Comentário";
+$l['mybblog_comment_submit'] = "Salvar Comentário";
+$l['mybblog_comment_saved'] = "O comentário foi salvo!";
+
+$l['mybblog_article_delete'] = "Excluir artigo \"{1}\" e todo dado assossiado?";
+$l['mybblog_comment_delete'] = "Excluir comentário no artigo \"{1}\"?";
+$l['mybblog_delete_confirm'] = "Você realmente deseja excluir o dado selecionado?";
+$l['mybblog_deleted'] = "Dado selecionado excluído";
+
+$l['mybblog_article_no_title'] = "O seu título é inválido";
+$l['mybblog_article_no_content'] = "Você não inseriu um artigo";
+$l['mybblog_article_no_tags'] = "Você precisa ,ao menos, uma etiqueta";
+$l['mybblog_article_unknown'] = "Ocorreu um erro desconhecido ao salvar o seu artigo";
+
+$l['mybblog_invalid_article'] = "O artigo especificado não existe";
+$l['mybblog_invalid_comment'] = "O comentário especificado não existe";
+
+$l['mybblog_comment_no_content'] = "Você não adicionou nenhum comentário";
+
+$l['mybblog_tag_no_tag'] = "Etiqueta vazia";
