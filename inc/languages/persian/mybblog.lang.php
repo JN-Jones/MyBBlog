1ced2c57fa3d89ecc51bb7150c4f845bfa561686
ATofighi <alirtofighim@gmail.com>
2014-09-30 13:44:24 +0330

    Translate To Persian

---
+++ inc/languages/persian/mybblog.lang.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Persian Language
+ * By: AliReza_Tofighi - http://my-bb.ir
+*/
+
+$l['mybblog'] = "وبلاگ";
+$l['mybblog_deactivated'] = "وبلاگ مای بی‌بی فعال نیست";
+
+$l['mybblog_write'] = "نوشتن یک مقاله‌ی جدید";
+$l['edit'] = "ویرایش";
+
+$l['mybblog_articles_none'] = "هیچ مقاله‌ای یافت نشد";
+$l['mybblog_posted'] = "ارسال‌شده در {1} توسط {2}";
+$l['mybblog_comments'] = "{1} نظر";
+$l['mybblog_tags'] = "برچسب شده با {1}";
+
+$l['mybblog_new_article'] = "نوشتنن یک مقاله‌ی جدید";
+$l['mybblog_edit_article'] = "ویرایش مقاله";
+$l['mybblog_title'] = "عنوان";
+$l['mybblog_article_tags'] = "کلمه‌های کلیدی";
+$l['mybblog_tags_desc'] = "جداسازی با ویرگول";
+$l['mybblog_article'] = "مقاله";
+$l['mybblog_article_submit'] = "ذخیره‌ی مقاله";
+$l['mybblog_article_written'] = "مقاله با موفقیت ذخیره شد";
+
+$l['mybblog_article_comments'] = "نظرات";
+
+$l['mybblog_new_comment'] = "نوشتن یک نظر جدید";
+$l['mybblog_edit_comment'] = "ویرایش نظر";
+$l['mybblog_comment_submit'] = "ذخیره‌ی نظر";
+$l['mybblog_comment_saved'] = "نظر باموفقیت ذخیره شد";
+
+$l['mybblog_article_delete'] = "حذف مقاله‌ی «{1}» و همه‌ی محتواهای مربوط به آن؟";
+$l['mybblog_comment_delete'] = "حذف نظر در مقاله‌ی «{1}»?";
+$l['mybblog_delete_confirm'] = "آیا شما مطمئن هستید که می‌خواهید محتوای انتخاب شده را حذف نمائید؟";
+$l['mybblog_deleted'] = "محتوای انتخاب شده حذف شد";
+
+$l['mybblog_article_no_title'] = "شما یک عنوان معتبر را انتخاب نکرده‌اید";
+$l['mybblog_article_no_content'] = "شما مقاله‌ای را وارد نکرده‌اید";
+$l['mybblog_article_no_tags'] = "شما نیاز به حداقل یک کلمه‌ی کلیدی دارید";
+$l['mybblog_article_unknown'] = "یک خطای ناشناخته در ذخیره‌سازی مقاله رخ داده‌است";
+
+$l['mybblog_invalid_article'] = "مقاله‌ای که انتخاب کرده‌اید وجود ندارد";
+$l['mybblog_invalid_comment'] = "نظری که انتخاب کرده‌اید وجود ندارد";
+
+$l['mybblog_comment_no_content'] = "شما هیچ نظری را وارد نکرده‌اید";
+
+$l['mybblog_tag_no_tag'] = "این کلمه‌ی کلیدی خالی است";
